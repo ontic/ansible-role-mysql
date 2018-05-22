f0f7a53d088879a07d190a0a18c5d1421ba1571a
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 00:25:53 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed MySQL version.
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which mysql
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm mysql -V
